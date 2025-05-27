e56b1725ce1a7b4d3ceb4a44c8d33d918cb968cd
Anvesh Khode <anveshkhode7588@gmail.com>
2025-05-27 21:44:06 +0530

    Login button integration done, update env to match Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
-import AuthLayout from './layouts/AuthLayout.tsx';
-import Home from './pages/Home.tsx';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import NotFound from './pages/NotFound.tsx';
-import './index.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import AuthLayout from "./layouts/AuthLayout.tsx";
+import Home from "./pages/Home.tsx";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import NotFound from "./pages/NotFound.tsx";
+import "./index.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
@@ -25,4 +32,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App; 
+export default App;
